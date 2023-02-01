aa5ac53ba7220d745f97a3ec64767b824b42d12a
shermn <sherman.meireles@yahoo.com>
2023-02-01 16:40:00 +0000

    printf_u

--- ft_dec_to_hex.c
+++ ft_dec_to_hex.c
@@ -6,7 +6,7 @@
 /*   By: sgoncalv <sgoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:42:53 by sgoncalv          #+#    #+#             */
-/*   Updated: 2023/01/27 13:27:44 by sgoncalv         ###   ########.fr       */
+/*   Updated: 2023/02/01 16:39:12 by sgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,12 @@
 
 void ft_dec_to_hex(size_t dec)
 {
-    char    base[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    char *base;
     
+    base = "0123456789ABCDEF";
     if ((dec / 16) > 0)
     {
         ft_dec_to_hex((dec / 16));
     }
     write(1, &base[dec % 16], 1);
-    write(1, "\n", 1);
 }
-/*
-int main()
-{
-    ft_dec_to_hex(115);
-    write(1, "\n", 1);
-    return(0);
-}*/
--- ft_printf_u.c
+++ ft_printf_u.c
@@ -6,7 +6,7 @@
 /*   By: sgoncalv <sgoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 14:42:45 by sgoncalv          #+#    #+#             */
-/*   Updated: 2023/01/27 14:45:30 by sgoncalv         ###   ########.fr       */
+/*   Updated: 2023/02/01 16:09:51 by sgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 int ft_printf_u(unsigned int n)
 {
-    int len;
-	char	*str;
+	char	dec[11];
+	int		i;
+    int     len;
 
-	str = ft_itoa(n);
-	len = ft_strlen(str);
-	free(str);
-    ft_putnbr_fd(n, 1);
-    write(1,"\n", 1);
-    return(len);
-}
-
-int main()
-{
-    int s;
-    s = -6748;
-    ft_printf_u(s);
-    return(0);
+	i = -1;
+    len = 0;
+	while (++i < 10)
+		dec[i] = i + '0';
+	dec[i] = '\0';
+	if (n > 9)
+		len += ft_printf_u(n / 10);
+	n = n % 10;
+	ft_printf_c((dec[n]));
+	return (len);
 }
