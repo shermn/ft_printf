65d6c972e4bd6bc82c1d77854f00c19fd4ce9a1c
shermn <sherman.meireles@yahoo.com>
2023-02-02 18:29:00 +0000

    comitado

--- ft_printf.c
+++ ft_printf.c
@@ -6,38 +6,59 @@
 /*   By: sgoncalv <sgoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 20:38:08 by sgoncalv          #+#    #+#             */
-/*   Updated: 2023/01/09 23:42:28 by sgoncalv         ###   ########.fr       */
+/*   Updated: 2023/02/02 18:28:13 by sgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int check_arg(va_list arg, char c)
+int	print_esp(const char c, va_list *print)
 {
-    int len;
+	int		len;
 
-    len = 1;
-    if (c == '')
-    {
-        /* code */
-    }
-    
+	len = 1;
+	if (c == 'c')
+		ft_printf_c((va_arg((*print), int)));
+	else if (c == 's')
+		len = ft_printf_s((va_arg((*print), char *)));
+	else if (c == '%')
+		ft_putchar_fd(c, 1);
+	else if (c == 'p')
+		len = ft_printf_p((va_arg((*print), unsigned long int)), 0);
+	else if (c == 'd' || c == 'i')
+		len = ft_printf_di((va_arg((*print), int)));
+	else if (c == 'u')
+		len = ft_printf_u((va_arg((*print), unsigned int)), 0);
+	else if (c == 'x')
+		len = ft_printf_x((va_arg((*print), int)), 0);
+	else if (c == 'X')
+		len = ft_printf_xx((va_arg((*print), int)), 0);
+	return (len);
 }
-int ft_printf(const char *str, ...)
-{
-    va_list arg;
-    int i;
-    int len;
 
-    i = 0;
-    len = 0;
-    va_start(arg, str);
-    while (str[i] != '\0')
-    {
-        if(str[i] == '%')
-            {
-                i++;
-                len += 
-            }
-    }
+int	ft_printf(const char *format, ...)
+{
+	int		len;
+	char	*str;
+	va_list	print;
+
+	len = 0;
+	str = (char *) format;
+	va_start(print, format);
+	while (str[0] != '\0')
+	{
+		if (str[0] == '%')
+		{
+			str++;
+			len = print_esp(str[0], &print) + len;
+		}
+		else
+		{
+			ft_putchar_fd(str[0], 1);
+			len++;
+		}
+		str++;
+	}
+	va_end(print);
+	return (len);
 }
