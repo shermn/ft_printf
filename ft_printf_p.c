0b39b9aa37eb7272e128750b281dd636f0df5b9e
shermn <sherman.meireles@yahoo.com>
2023-02-07 14:22:18 +0000

    update

--- ft_printf_p.c
+++ ft_printf_p.c
@@ -12,37 +12,6 @@
 
 #include "ft_printf.h"
 
-/*int	ft_conversor(unsigned long int n, int len)
-{
-	char	hexa[17];
-	int		i;
-	char	s;
-
-	s = '0';
-	i = -1;
-	while (++i < 16)
-	{
-		if (i == 10)
-			s = 'W';
-		hexa[i] = i + s;
-	}
-	hexa[i] = '\0';
-	if (n > 15)
-		len = ft_conversor((n / 16), len);
-	n = n % 16;
-	len++;
-	ft_printf_c(hexa[n]);
-	return (len);
-}
-
-int	ft_printf_p(unsigned long int n, int len)
-{
-	ft_putstr_fd("0x", 1);
-	len = ft_conversor(n, len);
-	return (len + 2);
-}
-*/
-
 int	ft_printf_p(unsigned long d)
 {
 	int		lenght;
